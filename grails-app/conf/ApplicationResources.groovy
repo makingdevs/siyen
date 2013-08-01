74c990fefbf1d7eecd51efa5b163ebbdb7cae164
felipe <fjuarezmurillo@synergyj.com>
2013-08-01 17:20:56 -0500

    agregando el módulo de bootstrap

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -1,5 +1,10 @@
 modules = {
-    application {
-        resource url:'js/application.js'
-    }
+  application {
+    resource url:'js/application.js'
+  }
+
+  bootstrap {
+    resource url:'js/bootstrap/bootstrap.min.js'
+    resource url:'css/bootstrap/bootstrap.min.css'
+  }
 }
