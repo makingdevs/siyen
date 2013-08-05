c65224f92fa47b3dfbe1d2b8c8933a04c0b1e442
felipe <fjuarezmurillo@synergyj.com>
2013-08-05 12:50:12 -0500

    #1 agregando pruebas unitarias para el domain

--- test/unit/com/siyen/PuertoSpec.groovy
+++ test/unit/com/siyen/PuertoSpec.groovy
@@ -3,18 +3,78 @@ package com.siyen
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
-/**
- * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
- */
 @TestFor(Puerto)
 class PuertoSpec extends Specification {
 
-	def setup() {
-	}
+  void "Validando constraints"() {
+    setup:
+    def puerto = new Puerto(
+      clave : clave_,
+      puerto : puerto_,
+      estado : estado_,
+      direccion : direccion_)
 
-	def cleanup() {
-	}
+    when:
+      puerto.save()
 
-	void "test something"() {
-	}
+    then:
+      assert puerto.errors.allErrors*.code == expected
+
+    where:
+    clave_ | puerto_ | estado_ | direccion_ || expected
+    null   | null    | null    | null       || ["nullable"] * 4
+    ""     | ""      | ""      | ""         || ["blank"] * 3
+    "1"*5  | "1"*36  | "1"*31  | "1"*501    || ["size.toobig"] * 4
+    "1"*5  | "1"*36  | "1"*31  | ""         || ["size.toobig"] * 3
+  }
+
+  void "Validando unique constraint"() {
+    setup:
+    def puerto = new Puerto(
+      clave : clave_,
+      puerto : puerto_,
+      estado : estado_,
+      direccion : direccion_)
+    mockForConstraintsTests(Puerto, [puerto])
+
+    def puerto2 = new Puerto(
+      clave : clave_,
+      puerto : puerto_,
+      estado : estado_,
+      direccion : direccion_)
+
+    when:
+      puerto2.save()
+
+    then:
+      assert puerto2.errors.allErrors*.code == expected
+
+    where:
+    clave_ | puerto_ | estado_ | direccion_ || expected
+    "1"*4  | "1"*35  | "1"*30  | "1"        || ["unique"]
+  }
+
+  void "Validando unique constraint"() {
+    setup:
+    def puerto = new Puerto(
+      clave : clave_,
+      puerto : puerto_,
+      estado : estado_,
+      direccion : direccion_)
+    mockForConstraintsTests(Puerto, [puerto])
+
+    when:
+      puerto.save()
+
+    then:
+      assert puerto.errors.allErrors*.code == expected
+
+    where:
+    clave_ | puerto_           | estado_   | direccion_                                                                             || expected
+    "PVJ"  | "Puerto Vallarta" | "Jalisco" | "BLVD.FCO.MEDINA ASCENCIO. KM. 4.6 TERMINAL MARITIMA PUERTO VALLARTA, JAL. C.P. 48302" || []
+    "PVJ"  | "Puerto Vallarta" | "Jalisco" | ""                                                                                     || []
+  }
+
+
+
 }
