7fe12da25b809a65342de2d1d4bb3c725d72d872
felipe <fjuarezmurillo@synergyj.com>
2013-08-05 11:53:38 -0500

    #1 creando la estructura básica para el catálogo de los puertos

--- grails-app/domain/com/siyen/Puerto.groovy
+++ grails-app/domain/com/siyen/Puerto.groovy
@@ -2,6 +2,20 @@ package com.siyen
 
 class Puerto {
 
-    static constraints = {
-    }
+  String clave
+  String puerto
+  String estado
+  String direccion
+
+  static constraints = {
+    clave size:1..4, blank:false, unique:true
+    puerto size:1..35, blank:false
+    estado size:1..30, blank:false
+    direccion blank: false
+  }
+
+  static mapping = {
+   direccion type: 'text'
+  }
+  
 }
