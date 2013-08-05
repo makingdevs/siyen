c37146dc1fe2918dbfa28729deb2eb8410f72881
felipe <fjuarezmurillo@synergyj.com>
2013-08-05 16:35:18 -0500

    #3 agregando los atributos necesarios y sus constraints

--- grails-app/domain/com/siyen/Curso.groovy
+++ grails-app/domain/com/siyen/Curso.groovy
@@ -2,6 +2,16 @@ package com.siyen
 
 class Curso {
 
-    static constraints = {
-    }
+  String clave
+  String nombre
+  Integer duracion // dias
+  String libreta
+
+  static constraints = {
+    clave size:1..30, blank:false, unique:true
+    nombre size:1..255, blank:false
+    duracion blank:false, max:5
+    libreta size:1..1, blank:false
+  }
+
 }
