c261d8ce916c41b338161c895f3a2209b81729ca
felipe <fjuarezmurillo@synergyj.com>
2013-08-06 11:08:18 -0500

    #8 agregando emberjs, handlebars y cambiando require por dependsOn

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -4,7 +4,16 @@ modules = {
   }
 
   bootstrap {
-    resource url:'js/bootstrap/bootstrap.min.js'
+    dependsOn 'jquery'
     resource url:'css/bootstrap/bootstrap.min.css'
+    resource url:'js/bootstrap/bootstrap.min.js'
   }
+
+  ember {
+    dependsOn 'jquery'
+    resource url:'js/ember/ember-1.0.0-rc.6.1.js'
+    resource url:'js/ember/ember-data.js'
+    resource url:'js/ember/handlebars-1.0.0-rc.4.js'
+  }
+
 }
