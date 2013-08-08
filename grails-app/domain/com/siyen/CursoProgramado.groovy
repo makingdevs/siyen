8e557b616acf3ea03484ccb84dc7d1b81f4a8ab8
felipe <fjuarezmurillo@synergyj.com>
2013-08-08 10:21:42 -0500

    #12 agregando clase de dominio para la creación de los cursos

---
+++ grails-app/domain/com/siyen/CursoProgramado.groovy
@@ -0,0 +1,21 @@
+package com.siyen
+
+class CursoProgramado {
+
+  // idCurso verificar el número actual para comenzar ahí `ALTER TABLE tablename AUTO_INCREMENT = 1`
+  Date fechaDeInicio 
+  Date fechaDeTermino
+
+  Date dateCreated
+  Date lastUpdated
+
+  Puerto puerto
+  Curso curso
+  Instructor instructor
+
+  StatusCurso statusCurso = StatusCurso.NUEVO
+
+  static constraints = {
+  }
+
+}
