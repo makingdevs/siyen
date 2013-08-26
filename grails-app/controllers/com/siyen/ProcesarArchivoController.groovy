7654ca4f8f98b72cf7c6d3548919a71a6a9cb08a
felipe <fjuarezmurillo@synergyj.com>
2013-08-26 17:58:31 -0500

    #28 creando el controller para el procesamiento del archivo

---
+++ grails-app/controllers/com/siyen/ProcesarArchivoController.groovy
@@ -0,0 +1,27 @@
+package com.siyen
+
+import jxl.*
+
+class ProcesarArchivoController {
+
+  def procesarArchivo() {
+
+    log.debug "params : $params"
+    FileInputStream excelParaProcesar = params.file.inputStream
+    WorkbookSettings ws = new WorkbookSettings()
+    ws.setEncoding("Cp1252")
+    Workbook workbook = Workbook.getWorkbook(excelParaProcesar, ws)
+    Sheet sheet = workbook.getSheet(0)
+
+    def contenidoDeFilas = []
+
+    (1..sheet.getRows() - 1).each { fila ->
+      contenidoDeFilas << sheet.getRow( fila )*.getContents()
+    }
+
+    render(contentType:"text/json") {
+      [ contenidoDeFilas : contenidoDeFilas ]
+    }
+  }
+
+}
