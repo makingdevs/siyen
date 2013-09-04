a85517dd0dd351caf4fa45dfc6c6febd19f93077
felipe <fjuarezmurillo@synergyj.com>
2013-09-04 14:49:30 -0500

    #40 agregando numero de control al alumno, falta generarlo

--- grails-app/domain/com/siyen/Alumno.groovy
+++ grails-app/domain/com/siyen/Alumno.groovy
@@ -2,12 +2,14 @@ package com.siyen
 
 class Alumno {
 
+  String numeroDeControl // (II 0's ID).size() == 8
   String nombreCompleto
   String observaciones
 
   static belongsTo = [cursoProgramado : CursoProgramado]
 
   static constraints = {
+    numeroDeControl size:1..8, blank:false // cambiar a 8..8
     nombreCompleto size:1..255, blank:false
     observaciones size:1..500, blank: true, nullable: true
   }
