e17bc7ed835ecf53306e4f17d1bd72a8ec5d1e68
felipe <fjuarezmurillo@synergyj.com>
2013-09-04 14:49:51 -0500

    #40 generando un certificado patito

--- grails-app/controllers/com/siyen/CertificadoController.groovy
+++ grails-app/controllers/com/siyen/CertificadoController.groovy
@@ -11,22 +11,27 @@ class CertificadoController {
     log.debug "generando certificado para el curso con id ${params.id}"
 
     CursoProgramado cursoProgramado = CursoProgramado.get(params.id)
-
-    model.addAttribute("curso",curso);
-    model.addAttribute("puerto",puerto);
-    model.addAttribute("tipoCurso",tipoCurso);
-    model.addAttribute("usuario",usuario);
-    model.addAttribute("alumnos", curso.getAlumnos());
-    model.addAttribute("format", "pdf");
+    def reportData = []
+    def data = [:]
+    data.nombreCompleto = "nombreCompleto"
+    data.numeroControl = "numeroControl"
+    data.nombreDelCurso = "nombreDelCurso"
+    data.claveDelCurso = "claveDelCurso"
+    data.puertoNombre = "puertoNombre"
+    data.puertoEstado = "puertoEstado"
+    data.fechaDeInicio = "fechaDeInicio"
+    data.fechaDeTermino = "fechaDeTermino"
+    data.duracionDelCurso = 7
+    data.nombreDelInstructor = "nombreDelInstructor"
+    reportData << data
 
     def reportDef = new JasperReportDef(
       name: 'certificado.jrxml',
       fileFormat: JasperExportFormat.PDF_FORMAT,
-      reportData: [],
+      reportData: reportData,
       // parameters: [urlImagen: (pathService.obtenerGrailsHome() + "log_ebc_tc.png")]
       )
-    log.debug reportDef
-    def reporte = []// jasperService.generateReport(reportDef).toByteArray()
+    def reporte = jasperService.generateReport(reportDef).toByteArray()
 
     response.setHeader("Content-disposition", "attachment; filename=certificado.pdf")
     response.outputStream << reporte
@@ -34,5 +39,21 @@ class CertificadoController {
 
   }
 
+
+}
+
+
+class MyExpando extends Expando {
+
+  Map props = [:]
+
+  MyExpando(Map props) {
+    super(props)
+    this.props = props
+  }
+
+  def invokeMethod(String name, Object args) {
+    props[name.minus('get').toLowerCase()]
+  }
 
 }
