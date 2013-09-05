42d25deb905d06844a291ed9afea5222ff1e9df5
felipe <fjuarezmurillo@synergyj.com>
2013-09-05 10:06:58 -0500

    #41 refactorizando código... externalizarlo a un servicio para la creación de un alumno

--- grails-app/services/com/siyen/CursoProgramadoService.groovy
+++ grails-app/services/com/siyen/CursoProgramadoService.groovy
@@ -11,23 +11,31 @@ class CursoProgramadoService {
     cursoProgramado.instructor = Instructor.get(cmd.instructor)
     cursoProgramado.fechaDeTermino = fechaDeInicio.clone().plus( cursoProgramado.curso.duracion )
 
-    cmd.alumnos.each {
-      String prefijo = "II"
-      Integer numerosEnMatricula = 6
-
-      Integer id = (Alumno.createCriteria().get {
-        projections {
-            max "id"
-        }
-      } ?: 0) + 1
-      String numeroDeControl = prefijo + String.format("%0${numerosEnMatricula}d", id)
-
-      Alumno alumno = new Alumno( numeroDeControl:numeroDeControl, nombreCompleto : it.nombre_completo, observaciones : it.observaciones )
+    cmd.alumnos.each { alumnoData ->
+      Alumno alumno = generarAlumnoConParams(alumnoData)
       cursoProgramado.addToAlumnos(alumno)
     }
 
     cursoProgramado.save(failOnError:true)
     cursoProgramado
+  }
+
+  private Alumno generarAlumnoConParams( def alumnoData ) {
+    String numeroDeControl = generarNumeroDeControl()
+    Alumno alumno = new Alumno( numeroDeControl:numeroDeControl, nombreCompleto : alumnoData.nombre_completo, observaciones : alumnoData.observaciones )
+    alumno
+  }
+
+  private String generarNumeroDeControl() {
+    String prefijo = "II"
+    Integer numerosEnMatricula = 6
+    Integer id = (Alumno.createCriteria().get {
+      projections {
+        max "id"
+      }
+    } ?: 0) + 1
+    String numeroDeControl = prefijo + String.format("%0${numerosEnMatricula}d", id)
+    numeroDeControl
   }
 
 }
