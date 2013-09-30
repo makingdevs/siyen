caf05cf8a78d085a014439f5839d2a738a727ef4
felipe <fjuarezmurillo@synergyj.com>
2013-09-30 16:47:06 -0500

    coding #45 agergando la generación del QR y refactorizando el código

--- grails-app/services/com/siyen/CertificadoService.groovy
+++ grails-app/services/com/siyen/CertificadoService.groovy
@@ -1,27 +1,50 @@
 package com.siyen
+import grails.plugins.qrcode.*
 
 class CertificadoService {
+
+  def QRCodeService
+  def grailsApplication
 
   def poblarCertificado(Long cursoProgramadoId) {
     CursoProgramado cursoProgramado = CursoProgramado.get(cursoProgramadoId)
 
     def reportData = []
     cursoProgramado.alumnos.each { alumno ->
-      def data = [:]
-      data.nombreCompleto = alumno.nombreCompleto
-      data.numeroControl = alumno.numeroDeControl
-      data.nombreDelCurso = cursoProgramado.curso.nombre
-      data.claveDelCurso = cursoProgramado.curso.clave
-      data.puertoNombre = cursoProgramado.puerto.puerto
-      data.puertoEstado = cursoProgramado.puerto.estado
-      data.fechaDeInicio = cursoProgramado.fechaDeInicio
-      data.fechaDeTermino = cursoProgramado.fechaDeTermino
-      data.duracionDelCurso = cursoProgramado.curso.duracion
-      data.nombreDelInstructor =  cursoProgramado.instructor.nombre
-      reportData << data
+      reportData << obtenerDatosDelAlumno(cursoProgramado, alumno)
     }
 
     reportData    
+  }
+
+  private obtenerDatosDelAlumno(cursoProgramado, alumno) {
+    def data = [:]
+    data.nombreCompleto = alumno.nombreCompleto
+    data.numeroControl = alumno.numeroDeControl
+    data.nombreDelCurso = cursoProgramado.curso.nombre
+    data.claveDelCurso = cursoProgramado.curso.clave
+    data.puertoNombre = cursoProgramado.puerto.puerto
+    data.puertoEstado = cursoProgramado.puerto.estado
+    data.fechaDeInicio = cursoProgramado.fechaDeInicio
+    data.fechaDeTermino = cursoProgramado.fechaDeTermino
+    data.duracionDelCurso = cursoProgramado.curso.duracion
+    data.nombreDelInstructor =  cursoProgramado.instructor.nombre
+
+    data.qrImage = genearQRConElNumeroDeControl(alumno.numeroDeControl)
+
+    data
+  }
+
+  def genearQRConElNumeroDeControl(String numeroControl) {
+    String serverURL = grailsApplication.config.grails.serverURL.toString()
+    Map informacion = [:]
+    def participanteInfo = "${serverURL}/participanteInfo/${numeroControl}"
+    informacion.put("chs", "250x250")
+    informacion.put("cht", "qr")
+    informacion.put("chl", participanteInfo)
+    informacion.put("chld", "H|1")
+    informacion.put("choe", "UTF-8")
+    QRCodeService.createQRCode(informacion, null, null, null)
   }
 
 }
