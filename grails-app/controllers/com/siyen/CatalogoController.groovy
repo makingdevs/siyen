137cc13ed56ea6e460cf3f56e2057c27bbe46674
felipe <fjuarezmurillo@synergyj.com>
2013-11-01 16:25:18 -0600

    coding #63 agregando los elementos para el autocomplete

--- grails-app/controllers/com/siyen/CatalogoController.groovy
+++ grails-app/controllers/com/siyen/CatalogoController.groovy
@@ -5,17 +5,32 @@ class CatalogoController {
   def catalogoService
 
   def obtenerDatosDeCatalogos() {
-
     def instructoresDisponibles = catalogoService.obtenerInstructores()
     def cursosDisponibles = catalogoService.obtenerCursos()
     def puertosDisponibles = catalogoService.obtenerPuertos()
 
     render(contentType:"text/json") {
       [instructoresDisponibles : instructoresDisponibles,
-            cursosDisponibles : cursosDisponibles,
+             cursosDisponibles : cursosDisponibles,
             puertosDisponibles : puertosDisponibles]
     }
+  }
 
+  def obtenerDatosDeBusqueda() {
+    def cursos = Curso.list()
+    def instructores = Instructor.list()
+    def puertos = Puerto.list()
+    def alumnos = Alumno.list()
+
+    render(contentType:"text/json") {
+      [ nombresDeCursos       : cursos*.nombre,
+        clavesDeCursos        : cursos*.clave,
+        nombresDeInstructores : instructores*.nombre,
+        clavesDePuertos       : puertos*.clave,
+        nombresDeAlumnos      : alumnos*.nombreCompleto,
+        numerosDeControl      : alumnos*.numeroDeControl
+      ]
+    }
   }
 
 }
