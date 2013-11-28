38079ecea4319e782d64ebfd28765f04f1ee6210
felipe <fjuarezmurillo@synergyj.com>
2013-11-28 16:51:16 -0600

    coding #82 agregando la búsqueda por rango de fechas

--- grails-app/controllers/com/siyen/InformePeriodicoController.groovy
+++ grails-app/controllers/com/siyen/InformePeriodicoController.groovy
@@ -2,6 +2,25 @@ package com.siyen
 
 class InformePeriodicoController {
 
+  def searchableService
+
   def index() { }
+
+  def realizarInfome() {
+    Date desde = Date.parse("dd/MMM/yyyy", params.desde)
+    Date hasta = Date.parse("dd/MMM/yyyy", params.hasta)
+
+    def busquedaDeResultados = searchableService.search({
+      must( between("fechaDeInicio", desde, hasta, true) )
+    })
+
+    log.debug busquedaDeResultados.total
+    log.debug busquedaDeResultados.results
+
+    log.debug busquedaDeResultados.results.fechaDeInicio
+    log.debug busquedaDeResultados.results.fechaDeTermino
+
+    busquedaDeResultados.results
+  }
 
 }
