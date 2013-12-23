4903777f5a2f384456043590f7506fd10cf2c3ff
felipe <fjuarezmurillo@synergyj.com>
2013-12-23 13:52:34 -0600

    coding #91 agregando prueba para el informe

--- grails-app/services/com/siyen/InformePeriodicoService.groovy
+++ grails-app/services/com/siyen/InformePeriodicoService.groovy
@@ -7,8 +7,47 @@ class InformePeriodicoService {
   // params.libreta
   // params.anio
 
-  def obtenerDatosDeGraficacion(def datosDeBusqueda) {
-    datosDeBusqueda
+  def obtenerDatosDeGraficacion(anio, closurePuerto = null) {
+    def cursoProgramadoCriteria = CursoProgramado.createCriteria()
+    def resultados = cursoProgramadoCriteria.list {
+      sqlRestriction "year(fecha_de_inicio) = ${anio}"
+
+      closurePuerto?.delegate = delegate
+      closurePuerto?.call()
+    }
+
+    resultados
+  }
+
+  def agrupar(anio, relacion, propiedad) {
+    def data = obtenerDatosDeGraficacion(anio)
+    def agrupamiento = [:]
+    data.each { cursoProgramado ->
+      if(!agrupamiento.(cursoProgramado."$relacion"."$propiedad")) {
+        agrupamiento.(cursoProgramado."$relacion"."$propiedad") = 0
+      }
+      agrupamiento.(cursoProgramado."$relacion"."$propiedad") += 1
+    }
+
+    agrupamiento
+  }
+
+  def agrupar(anio, claveDelPuerto, relacion, propiedad) {
+    def puerto = Puerto.findByClave(claveDelPuerto)
+    def puertoCriteria = {
+      eq "puerto", puerto
+    }
+    def data = obtenerDatosDeGraficacion(anio, puertoCriteria)
+
+    def agrupamiento = [:]
+    data.each { cursoProgramado ->
+      if(!agrupamiento.(cursoProgramado."$relacion"."$propiedad")) {
+        agrupamiento.(cursoProgramado."$relacion"."$propiedad") = 0
+      }
+      agrupamiento.(cursoProgramado."$relacion"."$propiedad") += 1
+    }
+
+    agrupamiento
   }
 
 }
--- test/integration/com/siyen/InformePeriodicoServiceSpec.groovy
+++ test/integration/com/siyen/InformePeriodicoServiceSpec.groovy
@@ -6,15 +6,79 @@ class InformePeriodicoServiceSpec extends IntegrationSpec {
 
   def informePeriodicoService
 
-  def "Obteniendo los datos de graficación"() {
+  def "Obteniendo los datos de graficación para los puertos por año"() {
     setup : "Asignando los valores a graficar"
-      def anio = "2012"
+      def anio = 2012
 
     when : "Llamando al servicio de graficación"
-      def resultados = informePeriodicoService.obtenerDatosDeGraficacion(anio:anio)
+      def resultados = informePeriodicoService.agrupar(anio, "puerto", "clave")
 
     then :
-      resultados
+      resultados.size() == datosAGraficar.size()
+      resultados == datosAGraficar
+
+    where :
+      datosAGraficar << [
+        [
+          '0'     : 12,
+          'ACG'   : 206,
+          'ATL'   : 5,
+          'BABC'  : 2,
+          'BTBC'  : 1,
+          'CHJ'   : 8,
+          'CHX'   : 11,
+          'CSL'   : 38,
+          'CULS'  : 2,
+          'DF'    : 41,
+          'EJMC'  : 1,
+          'ENS'   : 387,
+          'GJJ'   : 2,
+          'GNBC'  : 7,
+          'GYS'   : 360,
+          'ICBC'  : 9,
+          'LCM'   : 24,
+          'LIBC'  : 2,
+          'LPB'   : 193,
+          'MZO'   : 174,
+          'MZS'   : 1480,
+          'PMCH'  : 13,
+          'PPS'   : 21,
+          'PVJ'   : 43,
+          'PZM'   : 4,
+          'RIN'   : 4,
+          'SBN'   : 40,
+          'SCO'   : 235,
+          'SFBC'  : 13,
+          'TOP'   : 57,
+          'VBEM'  : 4,
+          'YAS'   : 6,
+          'YUR'   : 1,
+          'ZIH'   : 36
+        ]
+      ]
+  }
+
+  def "Obteniendo los datos de graficación para los puertos por año y meses"() {
+    setup : "Asignando los valores a graficar"
+      def anio = 2012
+      def puerto = "GYS"
+
+    when : "Llamando al servicio de graficación"
+      def resultados = informePeriodicoService.agrupar(anio, puerto, "curso", "libreta")
+
+    then :
+      resultados.size() == datosAGraficar.size()
+      resultados == datosAGraficar
+
+    where :
+      datosAGraficar << [
+        [
+          A : 154,
+          B : 169,
+          C : 36,
+          D : 1
+        ]
+      ]
   }
 
 }
