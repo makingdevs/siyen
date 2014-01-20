1f2a19dafc59d75bc8b3c996576cb945167c8466
felipe <fjuarezmurillo@synergyj.com>
2014-01-20 12:16:49 -0600

    coding #99 refactorizando el código

--- grails-app/controllers/com/siyen/CertificadoController.groovy
+++ grails-app/controllers/com/siyen/CertificadoController.groovy
@@ -15,36 +15,42 @@ class CertificadoController {
     CursoProgramado cursoProgramado = CursoProgramado.get(params.id.toLong())
     def reportData = certificadoService.poblarCertificado(cursoProgramado)
 
-    def reportDef = new JasperReportDef(
-      name: 'certificado.jrxml',
-      fileFormat: JasperExportFormat.PDF_FORMAT,
-      reportData: reportData
-    )
-    def reporte = jasperService.generateReport(reportDef).toByteArray()
-    response.setHeader("Content-disposition", "attachment; filename=certificado.pdf")
-    response.outputStream << reporte
-
+    frenteDelCertificado(reportData, "certificado")
     notificacionService.enviarNotificacion('cursoProgramado.impresion', cursoProgramado)
-
-    response
   }
 
   def generarReversoParaCurso() {
     Long cursoProgramadoId = params.id.toLong()
     CursoProgramado cursoProgramado = CursoProgramado.get(cursoProgramadoId)
-    String claveDelCurso = cursoProgramado.curso.clave
 
-    def reversoReporte = new File( grailsApplication.config.jasper.dir.reports + "/${claveDelCurso}.pdf" )
-
-    response.setHeader("Content-disposition", "attachment; filename=${claveDelCurso}.pdf")
-    response.outputStream << reversoReporte.readBytes()
-    response
+    reversoParaElCurso(cursoProgramado)
   }
 
   def generarFrenteParaCursoPorAlumno() {
     Alumno alumno = Alumno.get(params.id.toLong())
     def reportData = certificadoService.poblarCertificadoParaElAlumno(alumno)
 
+    frenteDelCertificado(reportData, alumno.numeroDeControl)
+    notificacionService.enviarNotificacionDeAlumno("cursoProgramado.impresion_de", alumno)
+  }
+
+  def generarReversoParaCursoPorAlumno() {
+    Alumno alumno = Alumno.get(params.id.toLong())
+    CursoProgramado cursoProgramado = alumno.cursoProgramado
+
+    reversoParaElCurso(cursoProgramado)
+  }
+
+  private def reversoParaElCurso(CursoProgramado cursoProgramado) {
+    String claveDelCurso = cursoProgramado.curso.clave
+
+    def reversoReporte = new File( grailsApplication.config.jasper.dir.reports + "/${claveDelCurso}.pdf" )
+    response.setHeader("Content-disposition", "attachment; filename=${claveDelCurso}.pdf")
+    response.outputStream << reversoReporte.readBytes()
+    response
+  }
+
+  private def frenteDelCertificado(def reportData, String nombreDelCertificado) {
     def reportDef = new JasperReportDef(
       name: 'certificado.jrxml',
       fileFormat: JasperExportFormat.PDF_FORMAT,
@@ -52,23 +58,9 @@ class CertificadoController {
     )
 
     def reporte = jasperService.generateReport(reportDef).toByteArray()
-    response.setHeader("Content-disposition", "attachment; filename=${alumno.numeroDeControl}.pdf")
+    response.setHeader("Content-disposition", "attachment; filename=${nombreDelCertificado}.pdf")
     response.outputStream << reporte
 
-    // notificacionService.enviarNotificacion('cursoProgramado.reimpresion', alumno)
-
-    response
-  }
-
-  def generarReversoParaCursoPorAlumno() {
-    Alumno alumno = Alumno.get(params.id.toLong())
-    CursoProgramado cursoProgramado = alumno.cursoProgramado
-    String claveDelCurso = cursoProgramado.curso.clave
-
-    def reversoReporte = new File( grailsApplication.config.jasper.dir.reports + "/${claveDelCurso}.pdf" )
-
-    response.setHeader("Content-disposition", "attachment; filename=${claveDelCurso}.pdf")
-    response.outputStream << reversoReporte.readBytes()
     response
   }
 
