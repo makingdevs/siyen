c0df6e551e3d96f5e4dd7d4a5cd30cbcda75be65
felipe <fjuarezmurillo@synergyj.com>
2014-01-21 10:41:16 -0600

    coding #102 generando el script para imprimir el certificado que se buscó

--- web-app/js/emberApplication.js
+++ web-app/js/emberApplication.js
@@ -2,4 +2,17 @@
 (function() {
   window.App = Ember.Application.create();
 
+  $(function() {
+    return ($("body")).on("click", "#impresion", function() {
+      var id, urlBaseFrente, urlBaseReverso, urlFrente, urlReverso;
+      id = ($(this)).attr('name');
+      urlBaseFrente = ($('#frenteParaCurso')).val();
+      urlFrente = "" + urlBaseFrente + "/" + id;
+      urlBaseReverso = ($('#reversoParaCurso')).val();
+      urlReverso = "" + urlBaseReverso + "/" + id;
+      window.open(urlFrente);
+      return window.open(urlReverso);
+    });
+  });
+
 }).call(this);
