f990ea4302141e8d1fd6f56c52717cb6acb90366
felipe <fjuarezmurillo@synergyj.com>
2014-01-27 14:17:58 -0600

    coding #106 cambiando la forma de la visualización del reporte

--- grails-app/controllers/com/siyen/OficioController.groovy
+++ grails-app/controllers/com/siyen/OficioController.groovy
@@ -1,31 +1,6 @@
 package com.siyen
 
-import grails.plugin.rendering.pdf.*
-
 class OficioController {
-
-  def springSecurityService
-
-  def index() {
-    def puerto = Puerto.get(1)
-    def desde = Date.parse('dd/MMMM/yyyy', (new Date() - 2).format('dd/MMMM/yyyy'))
-    def hasta = Date.parse('dd/MMMM/yyyy', (new Date()).format('dd/MMMM/yyyy'))
-
-    def data = [:]
-    data.dirigidoA = "CAP. ALT. ANTONIO BURGUEÑO GUARDADO"
-    data.puesto = "CAPITAN DE PUERTO"
-    data.direccion = "CALZ. DIEGO MTZ. CORONA ZONA FRANCA EDIF. S.C.T. SEGUNDO PISO GUAYMAS, SON C.P. 85400"
-    data.deParteDe = "Cap. Alt. Sergio Rolando Osuna Osuna"
-
-    data.fechaActual = new Date().format("dd 'de' MMMM 'del' yyyy")
-    data.puertoFechaAbbr = "GYS_${new Date().format('ddMMyy')}"
-    data.puerto = "Guaymas, Sonora"
-    data.desde = desde.format("dd 'de' MMMM 'del' yyyy")
-    data.hasta = hasta.format("dd 'de' MMMM 'del' yyyy")
-    data.cursos = CursoProgramado.findAllByFechaDeInicioBetween(desde, hasta)
-
-    render(template: "/pdfs/oficio", model: [data:data])
-  }
 
   def generarOficio() {
     def puerto = Puerto.get(params.puertoDeEnvio.toLong())
@@ -43,7 +18,9 @@ class OficioController {
     data.puerto = "${puerto.puerto}, ${puerto.estado}"
     data.desde = desde.format("dd 'de' MMMM 'del' yyyy")
     data.hasta = hasta.format("dd 'de' MMMM 'del' yyyy")
-    data.cursos = CursoProgramado.findAllByFechaDeInicioBetween(desde, hasta)
+
+    def consulta = CursoProgramado.findAllByFechaDeInicioBetween(desde, hasta)
+    data.cursos = consulta.groupBy({ "${it.curso.nombre}-${it.curso.clave}" })
 
     renderPdf(template: "/pdfs/oficio", model: [data:data], filename: "oficio")
   }
