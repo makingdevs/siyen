8ad7a465735525c0df9d3f03000fa70a1228a983
sohjiro <felipe@makingdevs.com>
2014-02-27 10:54:24 -0600

    coding #117 obteniendo el hostname por medio de la configuración

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -2,15 +2,5 @@ import org.vertx.java.platform.impl.*
 import grails.util.Environment
 
 beans = {
-
-  switch(Environment.current) {
-    case Environment.DEVELOPMENT:
-    case Environment.TEST:
-      defaultPlatformManager(DefaultPlatformManager, 9090, "localhost")
-    break
-    case Environment.PRODUCTION:
-      defaultPlatformManager(DefaultPlatformManager, 9090, "ienpop.net")
-    break
-  }
-
+  defaultPlatformManager(DefaultPlatformManager, 9090,  grailsApplication.config.grails.app.vertx.hostname)
 }
