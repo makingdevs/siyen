a53e376e2f45d971d5713b75f63b527ad8091760
sohjiro <felipe@makingdevs.com>
2014-03-25 18:27:38 -0600

    coding #130 buscando a los cursos con los parámetros enviados

--- grails-app/controllers/com/siyen/EstadoDeCuentaController.groovy
+++ grails-app/controllers/com/siyen/EstadoDeCuentaController.groovy
@@ -3,9 +3,16 @@ package com.siyen
 class EstadoDeCuentaController {
 
   def index() {
-    def puertos = [ puertos : Puerto.findAll { it.activo } ]
-    def instructores = [ instructores : Instructor.findAll { it.activo } ]
-    [ instructores : instructores, puertos : puertos ]
+    [ puertos : Puerto.findAll { it.activo }, instructores : Instructor.findAll { it.activo } ]
   }
 
+  def generarEstadoDeCuenta() {
+    Date fechaDeInicio = Date.parse("dd/MMMMM/yyyy", params.fechaDeInicio)
+    Date fechaDeTermino = Date.parse("dd/MMMM/yyyy", params.fechaDeTermino)
+    def instructor = Instructor.get(params.instructor)
+    def puerto = Puerto.get(params.puerto)
+    def cursosProgramados = CursoProgramado.findAllByInstructorAndPuertoAndDateCreatedBetween(instructor, puerto, fechaDeInicio, fechaDeTermino)
+
+    render template:'estadoDeCuenta', model : [cursosProgramados : cursosProgramados]
+  }
 }
