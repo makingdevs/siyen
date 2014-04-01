a503953a458959d9d85f87343b673a16dff5904f
sohjiro <felipe@makingdevs.com>
2014-03-31 19:04:41 -0600

    coding #129 updating dbm

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -76,7 +76,7 @@ grails.project.dependency.resolution = {
 
     build ":tomcat:7.0.47"
 
-    runtime ":database-migration:1.3.2"
+    runtime ":database-migration:1.3.8"
 
     compile ':cache:1.0.1'
 
