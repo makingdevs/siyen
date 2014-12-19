d79a18cb7642c4e28d30b4d6f4b1149b30cd4579
humberto <humberto2do@gmail.com>
2014-12-19 12:42:57 -0600

    coding #135 se actualiza query para obtener cursos programados en base a la fecha de creación del alumno

--- grails-app/controllers/com/siyen/EstadoDeCuentaController.groovy
+++ grails-app/controllers/com/siyen/EstadoDeCuentaController.groovy
@@ -11,8 +11,14 @@ class EstadoDeCuentaController {
     Date fechaDeTermino = Date.parse("dd/MMMM/yyyy", params.fechaDeTermino)
     def user = User.get(params.usuario)
     def puerto = Puerto.get(params.puerto)
-    def cursosProgramados = CursoProgramado.findAllByUserAndPuertoAndDateCreatedBetween(user, puerto, fechaDeInicio, fechaDeTermino)
-
+    def c = CursoProgramado.createCriteria()
+    def cursosProgramados = c.listDistinct{
+      alumnos{
+        between('dateCreated',fechaDeInicio,fechaDeTermino)
+      }
+      eq 'user.id',user.id
+      eq 'puerto.id',puerto.id
+    }
     render template:'estadoDeCuenta', model : [ cursosProgramados : cursosProgramados, fechaDeInicio : fechaDeInicio, fechaDeTermino : fechaDeTermino, puerto : puerto, user : user ]
   }
 }
