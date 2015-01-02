9d190901120ff57cbd4096f5f5d2ef7377f85cbd
humberto <humberto2do@gmail.com>
2015-01-02 17:30:53 -0600

    coding #129 se hacen cambios por actualización de grails a 2.3.3

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -7,9 +7,19 @@ grails.project.source.level = 1.7
 grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 // uncomment (and adjust settings) to fork the JVM to isolate classpaths
-//grails.project.fork = [
-//   run: [maxMemory:1024, minMemory:64, debug:false, maxPerm:256]
-//]
+grails.project.fork = [
+    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
+    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+
+    // configure settings for the test-app JVM, uses the daemon by default
+    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+    // configure settings for the run-app JVM
+    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the run-war JVM
+    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the Console UI JVM
+    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+]
 
 grails.project.dependency.resolution = {
   // inherit Grails' default dependencies
@@ -48,8 +58,6 @@ grails.project.dependency.resolution = {
     compile 'io.vertx:vertx-platform:2.0.0-final'
     compile 'io.vertx:vertx-core:2.0.0-final'
 
-    test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
-
     runtime 'org.springframework:spring-test:3.2.5.RELEASE'
     runtime 'org.xhtmlrenderer:core-renderer:R8'
     runtime 'com.lowagie:itext:2.1.0'
@@ -69,10 +77,6 @@ grails.project.dependency.resolution = {
       exclude 'poi'
       exclude 'jackson-core-asl'
       excludes 'itext', 'itext-rtf'
-    }
-
-    test(":spock:0.7") {
-      exclude "spock-grails-support"
     }
 
     compile(":rendering:0.4.4") {
