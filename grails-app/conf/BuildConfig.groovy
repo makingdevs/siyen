4a99431c6db484d8ffe4e69de28d376d096f250a
humberto <humberto2do@gmail.com>
2015-01-06 10:41:59 -0600

    coding #129 se hacen cambios al building por actualizar grails a 2.3.3

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -7,20 +7,15 @@ grails.project.source.level = 1.7
 grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 // uncomment (and adjust settings) to fork the JVM to isolate classpaths
+forkConfig = [maxMemory: 1024, minMemory: 64, debug: false, maxPerm: 256]
 grails.project.fork = [
-    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
-    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
-
-    // configure settings for the test-app JVM, uses the daemon by default
-    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
-    // configure settings for the run-app JVM
-    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-    // configure settings for the run-war JVM
-    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-    // configure settings for the Console UI JVM
-    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+  test: false,
+  run: forkConfig,
+  war: forkConfig,
+  console: forkConfig
 ]
-
+grails.reload.enabled = true
+grails.project.dependency.resolver = "maven"
 grails.project.dependency.resolution = {
   // inherit Grails' default dependencies
   inherits("global") {
@@ -93,7 +88,7 @@ grails.project.dependency.resolution = {
 
     compile ":qrcode:0.6"
 
-    compile ":searchable:0.6.9"
+    compile ":searchable:0.6.5"
 
     compile(":grails-melody:1.49.2") {
       excludes 'itext'
