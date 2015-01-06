1078e7a20dabf9cbe6f5e48a7cf612c138174795
humberto <humberto2do@gmail.com>
2015-01-06 10:43:42 -0600

    coding #129 se hacen cambios a la forma de declarar los beans por la version de grails

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -2,5 +2,8 @@ import org.vertx.java.platform.impl.*
 import grails.util.Environment
 
 beans = {
-  defaultPlatformManager(DefaultPlatformManager, 9090,  grailsApplication.config.grails.app.vertx.hostname)
+  defaultPlatformManager(DefaultPlatformManager){
+    9090
+    grailsApplication.config.grails.app.vertx.hostname
+  }
 }
