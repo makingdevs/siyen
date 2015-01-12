8a6b7c1a8ac8d21aea0e2e704d6588091a4a89e2
humberto <humberto2do@gmail.com>
2015-01-12 17:03:04 -0600

    coding #139 se agregan actions al controller para manejar respuesta y confirmación de payU

--- grails-app/controllers/com/siyen/PayUController.groovy
+++ grails-app/controllers/com/siyen/PayUController.groovy
@@ -3,4 +3,13 @@ package com.siyen
 class PayUController {
 
     def index() { }
+
+    def response(){
+      log.debug "respuesta del sistema payU --- ${params}"
+    }
+
+    def confirmation(){
+      log.debug "confirmación del sistema payU -- ${params}"
+    }
+
 }
