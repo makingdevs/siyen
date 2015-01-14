597f14b25107d76de58e550e7c363df21a714193
humberto <humberto2do@gmail.com>
2015-01-14 16:47:37 -0600

    coding #139 se agregan validaciones a respuesta de payu

--- grails-app/controllers/com/siyen/PayUController.groovy
+++ grails-app/controllers/com/siyen/PayUController.groovy
@@ -2,20 +2,38 @@ package com.siyen
 
 class PayUController {
 
-    def index() { 
-      [ 
-        cursos : Curso.findAll { activo == true }.collect{it.nombre},
-        referenceCode : "PAYU_RC_${new Date().format('dd-MM-yyyy-HH-mm')}_${Math.abs(new Random().nextInt() % 600) + 1}"
-      ]
+  def payUService
+
+  def index() { 
+    [ 
+      cursos : Curso.findAll { activo == true }.collect{it.nombre},
+      referenceCode : "PAYU_RC_${new Date().format('dd-MM-yyyy-HH-mm')}_${Math.abs(new Random().nextInt() % 600) + 1}"
+    ]
+  }
+
+  def response(PayUResponseCommand payUResponseCommand){
+
+    if(payUResponseCommand.hasErrors()){
+      render (status : 400, contentType:"text/json") {
+        [ errors : payUResponseCommand.properties ]
+      }
+      return
     }
 
-    def response(PayUResponseCommand payUResponseCommand){
-      log.debug "respuesta del sistema payU --- ${payUResponseCommand}"
-      render(view: "responsePayU", model: [payUResponse: payUResponseCommand])
+    def payU = payUService.guardarRespuestaPayU(payUResponseCommand)
+
+    if(!payU){
+      render (status : 500, contentType:"text/json") {
+        [ errors : "no se guardaron datos de la transaccion" ]
+      }
+      return
     }
 
-    def confirmation(PayUConfirmationCommand payUConfirmationCommand){
-      log.debug "confirmación del sistema payU -- ${payUConfirmationCommand}"
-    }
+    render(view: "responsePayU", model: [payUResponse: payUResponseCommand])
+  }
+
+  def confirmation(PayUConfirmationCommand payUConfirmationCommand){
+    log.debug "confirmación del sistema payU -- ${payUConfirmationCommand}"
+  }
 
 }
