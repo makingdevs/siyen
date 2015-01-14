55f3a484861f46291841b7467a3bd1c8287ed68e
humberto <humberto2do@gmail.com>
2015-01-14 12:53:04 -0600

    coding #139 se crea dominio para guardar detalles de las transacciones de payu

---
+++ grails-app/domain/com/siyen/PayU.groovy
@@ -0,0 +1,32 @@
+package com.siyen
+
+class PayU {
+  
+  String merchantId
+  String transactionState
+  String referenceCode
+  String reference_pol
+  String signature
+  String polPaymentMethod
+  String polPaymentMethodType
+  String installmentsNumber
+  String TX_VALUE
+  String TX_TAX
+  String buyerEmail
+  String processingDate
+  String currency
+  String lapResponseCode
+  String lapPaymentMethodType
+  String lapTransactionState
+  String message
+  String authorizationCode
+  String transactionId
+  String trazabilityCode
+  String TX_ADMINISTRATIVE_FEE
+  String TX_TAX_ADMINISTRATIVE_FEE
+  String TX_TAX_ADMINISTRATIVE_FEE_RETURN_BASE
+
+  static contraints = {
+
+  }
+}
