8fa1dfc012ad4e822fa9e9694df7b1f517944248
humberto <humberto2do@gmail.com>
2015-01-15 15:58:40 -0600

    coding #139 se agregan campos a dominio PayU a consecuencia de la confirmación PayU

--- grails-app/domain/com/siyen/PayU.groovy
+++ grails-app/domain/com/siyen/PayU.groovy
@@ -3,6 +3,7 @@ package com.siyen
 class PayU {
   
   String merchantId
+  String description
   String transactionState
   String referenceCode
   String reference_pol
@@ -25,8 +26,31 @@ class PayU {
   String tx_administrative_fee
   String tx_tax_administrative_fee
   String tx_tax_administrative_fee_return_base
+  //campos extras por confirmacion PayU
+  String state_pol
+  String signConfirmation
+  String payment_method
+  String payment_method_type
+  String transaction_date
+  String test
+  String account_number_ach
+  String account_type_ach
+  String attempts
+  String bank_id
+  String commision_pol
+  String commision_pol_currency
+  String customer_number
+  String date
+  String error_code_bank
+  String error_message_bank
+  String payment_method_id
+  String payment_request_state
+  String response_message_pol
+  String transaction_bank_id
+
 
   static constraints = {
+
     transactionState blank: true, nullable: true
     reference_pol blank: true, nullable: true
     polPaymentMethod blank: true, nullable: true
@@ -47,5 +71,28 @@ class PayU {
     tx_administrative_fee blank: true, nullable: true
     tx_tax_administrative_fee blank: true, nullable: true
     tx_tax_administrative_fee_return_base blank: true, nullable: true
+
+    state_pol blank: true, nullable: true
+    signConfirmation blank: true, nullable: true
+    payment_method blank: true, nullable: true
+    payment_method_type blank: true, nullable: true
+    transaction_date blank: true, nullable: true
+    test blank: true, nullable: true
+    account_number_ach blank: true, nullable: true
+    account_type_ach blank: true, nullable: true
+    attempts blank: true, nullable: true
+    bank_id blank: true, nullable: true
+    commision_pol blank: true, nullable: true
+    commision_pol_currency blank: true, nullable: true
+    customer_number blank: true, nullable: true
+    date blank: true, nullable: true
+    error_code_bank blank: true, nullable: true
+    error_message_bank blank: true, nullable: true
+    payment_method_id blank: true, nullable: true
+    payment_request_state blank: true, nullable: true
+    response_message_pol blank: true, nullable: true
+    transaction_bank_id blank: true, nullable: true
+    description blank: true, nullable: true
+    signature blank: true, nullable: true
   }
 }
