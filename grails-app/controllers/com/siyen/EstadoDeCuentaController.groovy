1583b316aadd26f4f586bbe89e0e31d751e13cd6
humberto <humberto2do@gmail.com>
2015-02-05 13:10:32 -0600

    coding #145 se agrega objeto command para aplicar validaciones en la busqueda de estado de cuenta

--- grails-app/controllers/com/siyen/EstadoDeCuentaController.groovy
+++ grails-app/controllers/com/siyen/EstadoDeCuentaController.groovy
@@ -6,17 +6,36 @@ class EstadoDeCuentaController {
     [ puertos : Puerto.findAll { it.activo }, instructor : Instructor.findAll { it.activo } ]
   }
 
-  def generarEstadoDeCuenta() {
-    Date fechaDeInicio = Date.parse("dd/MMMMM/yyyy", params.fechaDeInicio)
-    Date fechaDeTermino = Date.parse("dd/MMMM/yyyy", params.fechaDeTermino)
-    def instructor = Instructor.get(params.instructor)
-    def puerto = Puerto.get(params.puerto)
+  def generarEstadoDeCuenta(BusquedaEstadoCuentaCommand cmd) {
+    if(cmd.hasErrors()){
+      render(text: "<div class='alert alert-error'><strong>Son requeridos todos los criterios de busqueda</strong></div>", contentType: "text/html", encoding: "UTF-8")
+      return
+    }
+    Date fechaDeInicio = Date.parse("dd/MMMMM/yyyy", cmd.fechaDeInicio)
+    Date fechaDeTermino = Date.parse("dd/MMMM/yyyy", cmd.fechaDeTermino)
+    def instructor = Instructor.get(cmd.instructor)
+    def puerto = Puerto.get(cmd.puerto)
     def c = CursoProgramado.createCriteria()
     def cursosProgramados = c.listDistinct{
       between("fechaDeInicio", fechaDeInicio, fechaDeTermino)
       eq 'instructor.id',instructor.id
       eq 'puerto.id',puerto.id
     }
+
     render template:'estadoDeCuenta', model : [ cursosProgramados : cursosProgramados, fechaDeInicio : fechaDeInicio, fechaDeTermino : fechaDeTermino, puerto : puerto, instructor : instructor ]
+  }
+}
+
+class BusquedaEstadoCuentaCommand {
+  String fechaDeInicio
+  String fechaDeTermino
+  Long instructor
+  Long puerto
+
+  static constraints = {
+    fechaDeInicio nullable: false
+    fechaDeTermino nullable: false
+    instructor nullable: false
+    puerto nullable: false
   }
 }
