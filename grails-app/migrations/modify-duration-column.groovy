19815ab037b4f15c581a1695969bcb4066618d83
sohjiro <felipe@makingdevs.com>
2016-11-20 13:48:07 -0600

    coding #151 changing duracion field type

--- grails-app/migrations/modify-duration-column.groovy
+++ grails-app/migrations/modify-duration-column.groovy
@@ -1,6 +1,5 @@
 databaseChangeLog = {
-
-	changeSet(author: "sohjiro (generated)", id: "modify-duration-column") {
-		// TODO add changes and preconditions here
-	}
+  changeSet(author: "sohjiro (generated)", id: "modify-duration-column") {
+    modifyDataType(tableName: "curso", columnName: "duracion", newDataType: "double")
+  }
 }
