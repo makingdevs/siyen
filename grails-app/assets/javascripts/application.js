f8a7ac7ca858b59e1047295e3a85887c789db21d
Sohjiro <felipe@makingdevs.com>
2017-08-19 00:06:13 -0500

    adding more assets

---
+++ grails-app/assets/javascripts/application.js
@@ -0,0 +1,8 @@
+//= require emberApplication/application.js
+//= require emberApplication/router/router.js
+//= require emberApplication/controller/controller.js
+//= require emberApplication/model/store.js
+//= require emberApplication/model/model.js
+//= require emberApplication/helper/helper.js
+//= require emberApplication/helper/templates.js
+//= require emberApplication/view/view.js
