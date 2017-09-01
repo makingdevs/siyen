fab8972ba72df2bd294dfad99431299d10d83a45
sohjiro <felipe@makingdevs.com>
2017-08-31 23:05:06 -0500

    adding vertx bean for getting one instance of vertx in the application

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -2,4 +2,8 @@ import com.siyen.UserPasswordEncoderListener
 // Place your Spring DSL code here
 beans = {
   userPasswordEncoderListener(UserPasswordEncoderListener, ref('hibernateDatastore'))
+
+  vertx(io.vertx.core.Vertx) { bean ->
+    bean.factoryMethod = "vertx"
+  }
 }
